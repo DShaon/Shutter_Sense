b24d81ee237c57b195b39ed1e48676fd56543d01
unknown <toxicshaon@gmail.com>
2023-06-07 23:13:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl">hi fidasfjhij</h1>
+      <button className=" btn btn-info">dfgdsg</button>
+    </>
+  );
+}
+
+export default App;
